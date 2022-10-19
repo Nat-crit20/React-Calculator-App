dc5a82a5fcacc3b45c2d6197c8f06daa6ba3d9c7
Nathan Greuel <greuelnathan74@gmail.com>
2022-10-19 12:21:02 -0500

    Finished calcuation operations for the calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [total, setTotal] = useState("");
+  const [prevValue, setPrevValue] = useState("");
+  const [operation, setOperation] = useState("");
+
+  const handleChange = (e) => {
+    const value = e.target.innerText;
+    setTotal((state) => {
+      return state + value;
+    });
+  };
+
+  const handlePrevState = (e) => {
+    let prevState = total;
+    if (!prevValue) {
+      setOperation(e.target.innerText);
+      setPrevValue(prevState);
+      setTotal("");
+    } else {
+      calc();
+    }
+  };
+  const handleReset = () => {
+    setOperation("");
+    setPrevValue("");
+    setTotal("");
+  };
+  const handleDelete = () => {
+    setTotal("");
+  };
+  const calc = () => {
+    let numTotal = Number(total);
+    let numPrevValue = Number(prevValue);
+    setPrevValue();
+    let finalTotal = mathOperation(operation, numPrevValue, numTotal);
+    setOperation("");
+    setTotal(finalTotal);
+  };
+
+  const mathOperation = (operand, x, y) => {
+    switch (operand) {
+      case "+":
+        return x + y;
+      case "-":
+        return x - y;
+      case "/":
+        return x / y;
+      case "*":
+        return x * y;
+      case "=":
+        calc();
+        break;
+      default:
+        return;
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="display">
+        <div>prev {prevValue}</div>
+        <div>{operation}</div>
+        <div>current-{total}</div>
+      </div>
+      <div className="button-display">
+        <button onClick={handleChange}>7</button>
+        <button onClick={handleChange}>8</button>
+        <button onClick={handleChange}>9</button>
+        <button onClick={handleDelete} className="blue-highlight">
+          DEL
+        </button>
+        <button onClick={handleChange}>4</button>
+        <button onClick={handleChange}>5</button>
+        <button onClick={handleChange}> 6</button>
+        <button onClick={handlePrevState}>+</button>
+        <button onClick={handleChange}>1</button>
+        <button onClick={handleChange}>2</button>
+        <button onClick={handleChange}>3</button>
+        <button onClick={handlePrevState}>-</button>
+        <button onClick={handleChange}>.</button>
+        <button onClick={handleChange}>0</button>
+        <button onClick={handlePrevState}>/</button>
+        <button onClick={handlePrevState}>*</button>
+        <button onClick={handleReset} className="span-two blue-highlight">
+          RESET
+        </button>
+        <button onClick={handlePrevState} className="span-two red-highlight">
+          =
+        </button>
+      </div>
     </div>
   );
 }
